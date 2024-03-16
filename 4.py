d0965057098beff00b0595873bb1c925bd161b12
tizerr <iliya@plessky.ru>
2024-03-16 16:48:50 +0300

    task 4

---
+++ 4.py
@@ -0,0 +1,28 @@
+import csv
+
+
+def promo(info):
+    """Функция для генерации промокодов к товарам
+
+    info - список с информацией о товаре
+    """
+    cat, name, date, price, count = info
+    day, month, year = date.split('.')
+    promocode = name[:2] + day + name[-1:-3:-1] + month[::-1]
+    return promocode.upper()
+
+
+with open('products.csv', encoding='utf-8-sig', newline='\n') as f:
+    r = list(csv.reader(f, delimiter=';'))
+    head = r[0]
+    lst = r[1:]
+
+    promos = [promo(i) for i in lst]
+
+    head.append('promocode')
+    lst = [line + [promo] for line, promo in zip(lst, promos)]
+
+    with open('product_promo.csv', 'w', encoding='utf-8-sig') as fo:
+        w = csv.writer(fo, delimiter=';', lineterminator='\n')
+        w.writerow(head)
+        w.writerows(lst)
